abce0ba15c80c2ff2e06bdc92f6bb74289f56426
netzebandito <blake.netzeband@gmail.com>
2019-04-11 18:37:52 -0600

    added synchopation and animation for random house colors

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -26,25 +26,68 @@ export default {
 			imageCount: 7,
 			colorVariations: 2,
 			colorIndex: 0,
-			timeoutIDs: []
+			timeoutIDs: [],
+			interval: 500, // interval between images
+			syncopation: 250
 		};
 	},
 	methods: {
 		handleLoad(i) {
 			// add 1 to account for original image
 			if (i === this.imageCount * (this.colorVariations + 1)) {
+				// all images are loaded! remove css hidden class
 				this.showImages();
 			}
 		},
 		showImages() {
-			_.forIn(this.$refs, (val, key) => {
+			// only remove natural images
+			for (let i = this.imageCount; i > 0; i--) {
+				// get element from $refs
+				const element = this.$refs[i] ? this.$refs[i][0] : null;
+				const id = window.setTimeout(() => {
+					// last image, animate shit
+					if (i === this.imageCount) {
+						this.showBackgroundImages();
+						this.initImageAnimation();
+					}
+					return element.classList.remove("hidden");
+				}, this.interval * i);
+
 				// save id to destroy timeout on unmount
-				const id = window.setTimeout(
-					() => val[0].classList.remove("hidden"),
-					500 * key
-				);
 				this.timeoutIDs.push(id);
-			});
+			}
+		},
+		initImageAnimation() {
+			// animate 50 times
+			for (var i = 100; i >= 0; i--) {
+				const id = window.setTimeout(() => {
+					// get random image, layer, and  array of image variations
+					const imageIndex = _.random(1, this.imageCount);
+					const layerIndex = _.random(0, this.colorVariations);
+					const imageArray = Array.from(
+						{ length: this.colorVariations + 1 },
+						(val, i) => imageIndex + this.imageCount * i
+					);
+					// hide all except imageIndex
+					imageArray.forEach((refIndex, i) => {
+						if (i === layerIndex) {
+							this.$refs[refIndex][0].classList.remove("hidden");
+						} else {
+							this.$refs[refIndex][0].classList.add("hidden");
+						}
+					});
+				}, this.interval + this.getSyncopation() * i);
+				// save id for when component is destroyed
+				this.timeoutIDs.push(id);
+			}
+		},
+		getSyncopation() {
+			return _.random(0, 1) === 1
+				? this.interval / 2
+				: this.interval / 2 - 1;
+		},
+		showBackgroundImages() {
+			// place to load anxillary images
 		},
 		getImagePath(i) {
 			const isLastOfSeries = i % this.imageCount === 0;
@@ -58,14 +101,12 @@ export default {
 					isLastOfSeries ? numberOfSeries - 1 : numberOfSeries
 				}.png`;
 		},
-		getColor(i) {
-			return this.colors[parseInt(i / this.imageCount)];
-		},
 		getImageCount() {
 			return this.imageCount * (this.colorVariations + 1);
 		}
 	},
-	destroyed() {
+	beforeDestroyed() {
+		// kill any remaining timouts
 		this.timeoutIDs.forEach(id => window.clearTimeout(id));
 	}
 };
